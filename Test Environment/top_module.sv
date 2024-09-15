b736c4b5875e0c12f14ea4a73d8c1dbd2755b011
sriniverse004 <nandansrinivaas2004@gmail.com>
2024-09-16 03:05:26 +0530

    Update top_module.sv

--- Test Environment/top_module.sv
+++ Test Environment/top_module.sv
@@ -5,13 +5,27 @@
 module top_module();
 
 bit wr_clk, rd_clk;
+bit rst, wr_en, rd_en;
 
 always #50 wr_clk=~wr_clk;
 always #100 rd_clk=~rd_clk;
 
-inter intf(wr_clk, rd_clk);
+initial begin 
+ 
+  fork
+  
+  	#10 rst=1;
+  	#15 rst=0;
+  	#20 wr_en=1;
+  	#20 rd_en=1;
+    
+  join
+  
+end
 
-asyn_fifo f1(.wr_clk(wr_clk), .rd_clk(rd_clk), .rst(rst), .wr_en(wr_en), .rd_en(rd_en), .wdata(wdata), .rdata(rdata), .empty(empty), .full(full), .fifo_counter(fifo_counter));
+inter intf(wr_clk, rd_clk, rst, wr_en, rd_en);
+
+asyn_fifo f1(.wr_clk(intf.wr_clk), .rd_clk(intf.rd_clk), .rst(intf.rst), .wr_en(intf.wr_en), .rd_en(intf.rd_en), .wdata(intf.wdata), .rdata(intf.rdata), .empty(intf.empty), .full(intf.full), .fifo_counter(intf.fifo_counter));
 
 test tb(intf);
 
