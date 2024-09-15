5091fca2ae28ab82c9ce460778f2d4980314e02e
sriniverse004 <nandansrinivaas2004@gmail.com>
2024-09-16 03:13:30 +0530

    Create scoreboard.sv

---
+++ Test Environment/scoreboard.sv
@@ -0,0 +1,38 @@
+`include "transactor.sv"
+
+class scoreboard;
+
+    transactor trans;
+    virtual intf vif;
+    mailbox rec2sb;
+    bit [7:0] fifo_mem [15:0];
+    bit [3:0] rd_pntr;
+    
+    function new (virtual intf vif, mailbox rec2sb);
+        this.vif = vif;
+        this.rec2sb = rec2sb;
+    endfunction
+    
+    task main();
+    
+        forever begin;
+            
+            trans = new();
+            rec2sb.get(trans);
+            
+            if(trans.rd_en)begin
+                 if(trans.rdata == fifo_mem[rd_pntr])begin
+                     rd_pntr++;
+                     $display("Successful");
+                 end
+                 else begin
+                     $display("Failed");
+                 end
+    end
+
+            
+        end
+    
+    endtask
+
+endclass
