c43a356d7458d775d99bab76d94bbc8d325b748a
sriniverse004 <nandansrinivaas2004@gmail.com>
2024-09-16 03:11:56 +0530

    Create receiver.sv

---
+++ Test Environment/receiver.sv
@@ -0,0 +1,35 @@
+`include "transactor.sv"
+
+class receiver;
+
+    transactor trans;
+    virtual intf vif;
+    mailbox rec2sb;
+    
+    function new (virtual intf vif, mailbox rec2sb);
+        this.vif = vif;
+        this.rec2sb = rec2sb;
+    endfunction
+    
+    task  main();
+        
+        repeat(32)@(posedge vif.rd_clk)
+        begin
+            
+            trans = new();
+            
+            trans.wdata <= vif.wdata;
+            trans.fifo_counter <= vif.fifo_counter;
+            trans.rdata <= vif.rdata;
+            trans.empty <= vif.empty;
+            trans.full <= vif.full;
+            
+            rec2sb.put(trans);
+            
+            trans.display("Receiver");
+            
+        end
+        
+    endtask
+
+endclass
