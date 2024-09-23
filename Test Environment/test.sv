1afe6d4d13a22395595db5bf35afde17b4a04c88
sriniverse004 <nandansrinivaas2004@gmail.com>
2024-09-23 14:37:25 +0530

    Update test.sv

--- Test Environment/test.sv
+++ Test Environment/test.sv
@@ -5,6 +5,10 @@ program test(inter.mp1 intf);
 virtual intf vif;
 environment env;
 
+function new (virtual intf vif);
+        this.vif = vif;
+endfunction
+    
 initial begin
     env = new(virtual intf vif);
     env.start();
