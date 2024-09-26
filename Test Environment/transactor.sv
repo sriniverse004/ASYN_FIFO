7ad8c2047b7dda3aaf8c16a8944c51d6a6be445e
sriniverse004 <nandansrinivaas2004@gmail.com>
2024-09-26 15:35:24 +0530

    Update transactor.sv

--- Test Environment/transactor.sv
+++ Test Environment/transactor.sv
@@ -5,13 +5,19 @@ class transactor;
     bit [7:0] rdata;
     bit [3:0] fifo_counter;
     bit empty,full;
+  
+  	virtual intf vif;
+  
+  	function new (virtual intf vif);
+        this.vif = vif;
+    endfunction
 
     function void display (string name = "");
     
         $display("--------------------------------------------------");
         $display("%s", name);
         $display("--------------------------------------------------");
-        $display("rst=%0b, wr_en=%0b, rd_en=%0b", rst, wr_en, rd_en);
+      $display("rst=%0b, wr_en=%0b, rd_en=%0b", vif.rst, vif.wr_en, vif.rd_en);
         $display("wdata=%08b", wdata);
         $display("rdata=%08b", rdata);
         $display("fifo_counter=%0d", fifo_counter);
